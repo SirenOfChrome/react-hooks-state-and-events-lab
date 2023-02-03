9c3acd550497d8c45b41b93119e102ba7c217766
Forrest Jones <forrestjones2010@gmail.com>
2023-02-03 11:44:38 -0600

    GDC#4

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // state to keep track of whether the item is in the cart or not
+  const [inCart, setInCart] = useState(false);
+
+  // helper function to toggle the state of the item being in the cart or not
+  const toggleInCart = () => {
+    setInCart(!inCart);
+  };
+
+  // className to update the appearance of the item based on whether it's in the cart or not
+  const itemClass = inCart ? "in-cart" : "";
+
+  // button text to update based on whether the item is in the cart or not
+  const buttonText = inCart ? "Remove from Cart" : "Add to Cart";
+
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+    <li className={itemClass}>
+      <p>
+        {name} - {category}
+      </p>
+      <button onClick={toggleInCart}>{buttonText}</button>
     </li>
   );
 }
